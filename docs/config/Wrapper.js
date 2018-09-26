5ac9f05e5cc9a5bb9fab4d88e5189fa20773b84b
Greg Bergé <berge.greg@gmail.com>
2018-09-26 12:21:45 +0200

    chore: upgrade deps

--- docs/config/Wrapper.js
+++ docs/config/Wrapper.js
@@ -2,7 +2,9 @@
 import React from 'react'
 import { transparentize } from 'polished'
 import { ThemeProvider } from 'emotion-theming'
-import { theme, injectGlobal, globalStyle } from '@smooth-ui/core-sc'
+import { theme, th, injectGlobal, globalStyle } from '@smooth-ui/core-sc'
+
+const fromTheme = value => th(value)({ theme })
 
 injectGlobal`
   ${globalStyle()}
@@ -12,15 +14,15 @@ injectGlobal`
   }
 
   .sui-col {
-    border: 1px solid ${theme.primary};
-    background-color: ${transparentize(0.6, theme.primary)};
+    border: 1px solid ${fromTheme('primary')};
+    background-color: ${transparentize(0.6, fromTheme('primary'))};
     padding-top: .75rem;
     padding-bottom: .75rem;
   }
 
   .example-row-flex-cols .sui-row {
     min-height: 10rem;
-    background-color: ${transparentize(0.7, theme.primary)};
+    background-color: ${transparentize(0.7, fromTheme('primary'))};
   }
 `
 
